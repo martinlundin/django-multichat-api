d08cdf3433fc54b9d59b8427df2229c53166e74c
martinlundin <mlundin1994@gmail.com>
2019-03-01 21:07:19 +0100

    Added base64 input for image

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
 from . import models
 from rest_framework.authtoken.models import Token
+from drf_extra_fields.fields import Base64ImageField
+
 
 class UsernSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
     class Meta:
         model = models.Usern
         fields = ('uuid', 'username', 'name', 'image')
         read_only_fields = ('uuid', 'username')
 
+    def update(self, instance, validated_data):
+        instance.image = validated_data.pop('image')
+        instance.name = validated_data.pop('name')
+        instance.save()
+        return instance
 
 
 class TokenSerializer(serializers.ModelSerializer):

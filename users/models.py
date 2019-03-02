87702e820044ec9289ac29c0858bdf74aefde68d
martinlundin <mlundin1994@gmail.com>
2019-03-02 10:07:22 +0100

    Add default image to profile, and remove null/blank

--- users/models.py
+++ users/models.py
@@ -11,7 +11,7 @@ def get_image_path(instance, filename):
 class Usern(AbstractUser):
     uuid = models.UUIDField(primary_key=True, default=makeuuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    image = models.ImageField(upload_to=get_image_path, default="/general/default-profile.png")
 
     def __str__(self):
         return str(self.uuid)

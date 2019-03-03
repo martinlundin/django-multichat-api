ef2ab7c6058b9fc14c2b3a9d285fafe03c2c6481
martinlundin <mlundin1994@gmail.com>
2019-03-03 16:11:19 +0100

    Add permission file for users

--- users/views.py
+++ users/views.py
@@ -2,15 +2,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from . import models
 from . import serializers
-
-
-#Is this the right place to add permission classes?
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return str(obj.uuid) == str(request.user)
+from .permissions import IsOwnerOrReadOnly
 
 
 class UsernListView(generics.ListAPIView):
